23ac4012259a3231fad31d151ba492c52e9fed93
kiu_khalatiani.nikolai <khalatiani.nikolai@kiu.edu.ge>
2021-10-17 20:31:38 +0400

    done

--- src/pgdp/pinguart/PinguArt.java
+++ src/pgdp/pinguart/PinguArt.java
@@ -18,47 +18,30 @@ public class PinguArt extends MiniJava {
         writeConsole("|");
         int count = 1;
         while (count <= 8) {
-            if (count == 1)
-                x = userinput1;
-            else if (count == 2)
-                x = userinput2;
-            else if (count == 3)
-                x = userinput3;
-            else if (count == 4)
-                x = userinput4;
-            else if (count == 5)
-                x = userinput5;
-            else if (count == 6)
-                x = userinput6;
-            else if (count == 7)
-                x = userinput7;
-            else if (count == 8)
-                x = userinput8;
+            if (count == 1) x = userinput1;
+            else if (count == 2) x = userinput2;
+            else if (count == 3) x = userinput3;
+            else if (count == 4) x = userinput4;
+            else if (count == 5) x = userinput5;
+            else if (count == 6) x = userinput6;
+            else if (count == 7) x = userinput7;
+            else if (count == 8) x = userinput8;
             count++;
             while (x > 0) {
                 int repeat = 8;
                 while (repeat != 0) {
                     int digit = x % 10;
                     x = x / 10;
-                    if (digit == 1) {
-                        writeConsole("-");
-                    } else if (digit == 2) {
-                        writeConsole("~");
-                    } else if (digit == 3) {
-                        writeConsole("P");
-                    } else {
-                        writeConsole(" ");
-                    }
+                    if (digit == 1) writeConsole("-");
+                    else if (digit == 2) writeConsole("~");
+                    else if (digit == 3) writeConsole("P");
+                    else writeConsole(" ");
                     repeat--;
                 }
                 if (count % 2 == 1) {
-
                     writeConsole("|");
                     writeLineConsole();
-                    if ((count - 2) != 7) {
-                        writeConsole("|");
-                    }
-
+                    if ((count - 2) != 7) writeConsole("|");
                 }
             }
         }

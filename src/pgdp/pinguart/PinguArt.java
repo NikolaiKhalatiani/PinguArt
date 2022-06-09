726f6055a643167f6630196441faa5ab40047446
Nikolai Khalatiani <65181600+NikolaiKhalatiani@users.noreply.github.com>
2022-06-09 09:56:34 +0400

    Update PinguArt.java

--- src/pgdp/pinguart/PinguArt.java
+++ src/pgdp/pinguart/PinguArt.java
@@ -5,46 +5,46 @@ import pgdp.MiniJava;
 public class PinguArt extends MiniJava {
 
     public static void main(String[] args) {
+        /* This can be vastly improved if arrays are used, but we have to show here that
+         * it is possible without arrays as well.
+         */
         int x = 0;
-        int userinput1 = readInt("Please enter a Pingu Art number:");
-        int userinput2 = readInt("Please enter a Pingu Art number:");
-        int userinput3 = readInt("Please enter a Pingu Art number:");
-        int userinput4 = readInt("Please enter a Pingu Art number:");
-        int userinput5 = readInt("Please enter a Pingu Art number:");
-        int userinput6 = readInt("Please enter a Pingu Art number:");
-        int userinput7 = readInt("Please enter a Pingu Art number:");
-        int userinput8 = readInt("Please enter a Pingu Art number:");
-        write("+---[PinguArt]---+");
-        writeConsole("|");
+        // allow the user to input eight Pingu-Art numbers
+        int userInput1 = readInt("Please enter a Pingu Art number:");
+        int userInput2 = readInt("Please enter a Pingu Art number:");
+        int userInput3 = readInt("Please enter a Pingu Art number:");
+        int userInput4 = readInt("Please enter a Pingu Art number:");
+        int userInput5 = readInt("Please enter a Pingu Art number:");
+        int userInput6 = readInt("Please enter a Pingu Art number:");
+        int userInput7 = readInt("Please enter a Pingu Art number:");
+        int userInput8 = readInt("Please enter a Pingu Art number:");
+        System.out.println("+---[PinguArt]---+"); // print the Pingu-Art header
         int count = 1;
         while (count <= 8) {
-            if (count == 1) x = userinput1;
-            else if (count == 2) x = userinput2;
-            else if (count == 3) x = userinput3;
-            else if (count == 4) x = userinput4;
-            else if (count == 5) x = userinput5;
-            else if (count == 6) x = userinput6;
-            else if (count == 7) x = userinput7;
-            else if (count == 8) x = userinput8;
-            count++;
-            while (x > 0) {
-                int repeat = 8;
-                while (repeat != 0) {
-                    int digit = x % 10;
-                    x = x / 10;
-                    if (digit == 1) writeConsole("-");
-                    else if (digit == 2) writeConsole("~");
-                    else if (digit == 3) writeConsole("P");
-                    else writeConsole(" ");
-                    repeat--;
-                }
-                if (count % 2 == 1) {
-                    writeConsole("|");
-                    writeLineConsole();
-                    if ((count - 2) != 7) writeConsole("|");
-                }
+            if (count % 2 == 1) System.out.print("|");
+            if (count == 1) x = userInput1;
+            else if (count == 2) x = userInput2;
+            else if (count == 3) x = userInput3;
+            else if (count == 4) x = userInput4;
+            else if (count == 5) x = userInput5;
+            else if (count == 6) x = userInput6;
+            else if (count == 7) x = userInput7;
+            else if (count == 8) x = userInput8;
+            int digitPos = 1;
+            while (digitPos <= 8) {
+                int digit = x % 10;
+                if (digit == 0) System.out.print(" ");
+                else if (digit == 1) System.out.print("-");
+                else if (digit == 2) System.out.print("~");
+                else if (digit == 3) System.out.print("P");
+                x = x / 10;
+                digitPos++;
             }
+            if (count % 2 == 0) System.out.println("|");
+            count++;
         }
-        write("+----------------+");
+        System.out.println("+----------------+");
     }
 }
+
+
